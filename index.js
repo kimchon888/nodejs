78c1abe94ed3a100f8349f63056405a1999e2f81
phamn <anhvotinh500@gmail.com>
2024-09-14 22:00:42 +0700

    upload source code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello')
+import http, { request } from "node:http";
+import path from "node:path";
+
+const note =  "/users/joe/notes.txt";
+let name = "data.txt";
+let folder = "public";
+let a = path.join(folder,name);
+
+console.log(path.dirname(note));
+console.log(path.extname(note));
+console.log(a);
+
+http
+  .createServer((request, response)=>{
+    response.write('<h1>Hello World</h1>')
+    response.end();
+  })
+  .listen(3000);
